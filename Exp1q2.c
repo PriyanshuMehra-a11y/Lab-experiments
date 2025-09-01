b23225e64488f4fa9038fb624501320da169ea30
PriyanshuMehra-a11y <mehramillie4@gmail.com>
2025-09-01 15:38:39 +0530

    Add files via upload

---
+++ Exp1q2.c
@@ -0,0 +1,5 @@
+#include "stdio.h"
+int main(){
+    printf("Uttarakhand\nPithoragarh\n262501");
+    //Priyanshu
+}
---
+++ Exp1q3.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+int main(){
+int a;
+printf("Enter your age:");
+scanf("%d",&a);
+char n;
+printf("Enter your name:");
+scanf("%d",&n);
+//Priyanshu
+}
