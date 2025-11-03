5993458d1a6536a41d6fff27d6fdc95519a917d3
PriyanshuMehra-a11y <mehramillie4@gmail.com>
2025-11-03 14:39:01 +0530

    exp8.2.c

--- exp8.2.c
+++ exp8.2.c
@@ -10,25 +10,26 @@ int main() {
     char *ptr_char = &c;
 
     printf("INT POINTER:\n");
-    printf("Original address: %d, value: %d\n", ptr_int, *ptr_int);
+    printf("Original address: %p, value: %p\n", ptr_int, *ptr_int);
     ptr_int++;
-    printf("After increment:  %d (value unknown)\n", ptr_int);
+    printf("After increment:  %p (value unknown)\n", ptr_int);
     ptr_int--;
-    printf("After decrement:  %d, value: %d\n\n", ptr_int, *ptr_int);
+    printf("After decrement:  %p, value: %p\n\n", ptr_int, *ptr_int);
 
     printf("FLOAT POINTER:\n");
-    printf("Original address: %d, value: %f\n", ptr_float, *ptr_float);
+    printf("Original address: %p, value: %f\n", ptr_float, *ptr_float);
     ptr_float++;
-    printf("After increment:  %d (value unknown)\n", ptr_float);
+    printf("After increment:  %p (value unknown)\n", ptr_float);
     ptr_float--;
-    printf("After decrement:  %d, value: %f\n\n", ptr_float, *ptr_float);
+    printf("After decrement:  %p, value: %f\n\n", ptr_float, *ptr_float);
 
     printf("CHAR POINTER:\n");
-    printf("Original address: %d, value: %c\n", ptr_char, *ptr_char);
+    printf("Original address: %p, value: %c\n", ptr_char, *ptr_char);
     ptr_char++;
-    printf("After increment:  %d (value unknown)\n", ptr_char);
+    printf("After increment:  %p (value unknown)\n", ptr_char);
     ptr_char--;
-    printf("After decrement:  %d, value: %c\n", ptr_char, *ptr_char);
+    printf("After decrement:  %p, value: %c\n", ptr_char, *ptr_char);
 
     return 0;
+
 }
